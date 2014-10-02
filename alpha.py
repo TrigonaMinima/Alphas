add77fe6e12e0bc45d5e86894c627a02f3686ad0
nkmittal4994 <nkmittal4994@gmail.com>
2014-10-02 13:21:27 -0700

    added dicionary for bin,octal and hex for ascii values of alphabets

--- alpha.py
+++ alpha.py
@@ -20,9 +20,30 @@ def dicalpha():
     d[a] = ''
   return d 
 
+def dict_bi():
+  bi = {}
+  for a in alphlist():
+    bi[a] = "{0:b}".format(ord(a))  
+  return bi
+
+def dict_oct():
+  octa = {}
+  for a in alphlist():
+    octa[a] = "{0:o}".format(ord(a))  
+  return octa
+  
+def dict_hex():
+  hexa = {}
+  for a in alphlist():
+    hexa[a] = "{0:x}".format(ord(a))  
+  return hexa  
+
 if __name__ == '__main__':
   print loweralph()
   print upperalph()
   print alphlist()
   print dicalpha()
+  print dict_bi()
+  print dict_oct()
+  print dict_hex()
         
