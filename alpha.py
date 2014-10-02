9b22d5630c180a0efc4233d3232e4140884bd06c
TrigonaMinima <shivamrana95@yahoo.in>
2014-10-03 02:50:25 +0530

    proper newlines

--- alpha.py
+++ alpha.py
@@ -1,5 +1,6 @@
 import string
 
+
 def alph(start=0, end=-1):
     """
     alphas.alph([start, [end]]) -> str
@@ -7,7 +8,8 @@ def alph(start=0, end=-1):
     Returns a string of alphabets in lowercase from start till end. By default the start
     is 0 and end is 26.
     """
-    return string.ascii_lowercase[start:end]     
+    return string.ascii_lowercase[start:end]
+
 
 def ualph(start=0, end=-1):
     """
@@ -18,6 +20,7 @@ def ualph(start=0, end=-1):
     """
     return string.ascii_uppercase[start:end]
 
+
 def alphlist(start=0, end=-1):
     """
     alphas.alphlist([start, [end]]) -> list
@@ -25,7 +28,8 @@ def alphlist(start=0, end=-1):
     Returns a list of alphabets in lowercase from start till end. By default the start
     is 0 and end is 26.
     """
-    return list(alph(start, end))   
+    return list(alph(start, end))
+
 
 def ualphlist(start=0, end=-1):
     """
@@ -35,6 +39,7 @@ def ualphlist(start=0, end=-1):
     is 0 and end is 26.
     """
     return list(uAlph(start, end))
+
 
 def dicalph(value='', start=0, end=-1):
     """
@@ -50,6 +55,7 @@ def dicalph(value='', start=0, end=-1):
 
     return d
 
+
 def udicalph(value='', start=0, end=-1):
     """
     alphas.dicAlph([value, [start, [end]]]) -> dict
@@ -64,30 +70,34 @@ def udicalph(value='', start=0, end=-1):
 
     return d
 
+
 def dict_bi():
   bi = {}
   for a in alphlist():
-    bi[a] = "{0:b}".format(ord(a))  
+    bi[a] = "{0:b}".format(ord(a))
   return bi
+
 
 def dict_oct():
   octa = {}
   for a in alphlist():
-    octa[a] = "{0:o}".format(ord(a))  
+    octa[a] = "{0:o}".format(ord(a))
   return octa
-  
+
+
 def dict_hex():
   hexa = {}
   for a in alphlist():
-    hexa[a] = "{0:x}".format(ord(a))  
-  return hexa  
+    hexa[a] = "{0:x}".format(ord(a))
+  return hexa
 
-if __name__ == '__main__':
-  print loweralph()
-  print upperalph()
-  print alphlist()
-  print dicalpha()
-  print dict_bi()
-  print dict_oct()
-  print dict_hex()
-        
+
+# if __name__ == '__main__':
+#   print loweralph()
+#   print upperalph()
+#   print alphlist()
+#   print dicalpha()
+#   print dict_bi()
+#   print dict_oct()
+#   print dict_hex()
+
