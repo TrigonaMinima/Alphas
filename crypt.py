526535262d7c6fdcdf5a9f2dcfbca34d7c9e8b7d
TrigonaMinima <shivamrana95@yahoo.in>
2014-10-05 13:18:37 +0530

    unwanted function shifted, will be visible in next commit

--- crypt.py
+++ crypt.py
@@ -1,5 +1,5 @@
-from core import *
-from loremipsum import *
+import core
+
 
 def freqal(text):
     """
@@ -12,7 +12,7 @@ def freqal(text):
     extra = '1234567890~`!@#$%^&*()-=_+|}{[]:\";\'\\<>,.?/ '
     for i in extra:
         text = text.replace(i, '')
-    freq = dic(0)
+    freq = core.dic(0)
     for i in text:
         freq[i] += 1
 
@@ -28,11 +28,11 @@ def rot(rotv=13, start=0, end=26):
     be anything. By default the start is 0 and end is 26.
     """
     d = {}
-    for a in alphlist(start, end):
+    for a in core.alist(start, end):
         if (rotv + start < 26):
-          d[a] = alph(start+rotv,start+rotv+1)
+          d[a] = core.alph(start+rotv,start+rotv+1)
         else :
-          d[a] = alph(start+rotv-26,start+rotv-26+1)
+          d[a] = core.alph(start+rotv-26,start+rotv-26+1)
         start += 1
     return d
 
@@ -46,134 +46,10 @@ def urot(rotv=13, start=0, end=26):
     be anything. By default the start is 0 and end is 26.
     """
     d = {}
-    for a in ualphlist(start, end):
+    for a in core.ualist(start, end):
         if (rotv + start < 26):
-          d[a] = ualph(start+rotv,start+rotv+1)
+          d[a] = core.ualph(start+rotv,start+rotv+1)
         else :
-          d[a] = ualph(start+rotv-26,start+rotv-26+1)
+          d[a] = core.ualph(start+rotv-26,start+rotv-26+1)
         start += 1
-    return d   
-
-
-def braille(start=0, end=26):
-    """
-    crypt.braille([start, [end]]) -> dict
-
-    Returns a dictionary of alphabets (key is alphabet) and value is its braille
-    Note : Braille of uppercase and lowercase alphabets is same.
-    """
-    letters = {'a' : [[1, 0],
-                  [0, 0],
-                  [0, 0]],
-          
-           'b' : [[1, 0],
-                  [1, 0],
-                  [0, 0]],
-          
-           'c' : [[1, 1],
-                  [0, 0],
-                  [0, 0]],
-          
-           'd' : [[1, 1],
-                  [0, 1],
-                  [0, 0]],
-          
-           'e' : [[1, 0],
-                  [0, 1],
-                  [0, 0]],
-
-           'f' : [[1, 1],
-                  [1, 0],
-                  [0, 0]],
-
-           'g' : [[1, 1],
-                  [1, 1],
-                  [0, 0]],
-
-           'h' : [[1, 0],
-                  [1, 1],
-                  [0, 0]],
-
-           'i' : [[0, 1],
-                  [1, 0],
-                  [0, 0]],
-
-           'j' : [[0, 1],
-                  [1, 1],
-                  [0, 0]],
-
-           'k' : [[1, 0],
-                  [0, 0],
-                  [1, 0]],
-
-           'l' : [[1, 0],
-                  [1, 0],
-                  [1, 0]],
-
-           'm' : [[1, 1],
-                  [0, 0],
-                  [1, 0]],
-
-           'n' : [[1, 1],
-                  [0, 1],
-                  [1, 0]],
-
-           'o' : [[1, 0],
-                  [0, 1],
-                  [1, 0]],
-
-           'p' : [[1, 1],
-                  [1, 0],
-                  [1, 0]],
-
-           'q' : [[1, 1],
-                  [1, 1],
-                  [1, 0]],
-
-           'r' : [[1, 0],
-                  [1, 1],
-                  [1, 0]],
-
-           's' : [[0, 1],
-                  [1, 0],
-                  [1, 0]],
-          
-           't' : [[0, 1],
-                  [1, 1],
-                  [1, 0]],
-
-           'u' : [[1, 0],
-                  [0, 0],
-                  [1, 1]],
-
-           'v' : [[1, 0],
-                  [1, 0],
-                  [1, 1]],
-
-           'w' : [[0, 1],
-                  [1, 1],
-                  [0, 1]],
-
-           'x' : [[1, 1],
-                  [0, 0],
-                  [1, 1]],
-
-           'y' : [[1, 1],
-                  [0, 1],
-                  [1, 1]],
-
-           'z' : [[1, 0],
-                  [0, 1],
-                  [1, 1]]}
-    d = dic(0,start,end)
-    for i in d:
-        d[i] = letters[i]  
     return d
-
-
-def lipsum():
-    """
-    Returns string lorem ipsum
-    """
-    lipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    return lipsum
